680a39a2efccb986c7a1faba13ed1405d50d2471
El vudoo <164950159+vudoocyber@users.noreply.github.com>
2025-07-27 16:48:17 -0600

    Update actualizador_web.py

--- actualizador_web.py
+++ actualizador_web.py
@@ -30,7 +30,7 @@ def aplicar_reglas_html(texto_crudo):
         if not linea: continue
         if linea.startswith("Eventos Deportivos"):
             fecha_texto = linea.replace("Eventos Deportivos ", "").strip()
-            resultado_html += f"<h2>Eventos Deportivos, {year_actual} <br /><br />\n{fecha_texto} <br /><br /><br />\n"
+            resultado_html += f"<h2>Eventos Deportivos y Especiales, {year_actual} <br /><br />\n{fecha_texto} <br /><br /><br />\n"
         elif REGEX_EMOJI.search(linea) or "Evento BOX" in linea:
             resultado_html += f"<h3>{linea}</h3><br /><br />\n"
         elif any(keyword in linea for keyword in PALABRAS_CLAVE):
